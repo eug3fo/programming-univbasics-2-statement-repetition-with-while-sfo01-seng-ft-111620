a3959d763aa7a1b872788b124fff433645ac5193
Eugene Hong <eugenebhong@gmail.com>
2020-10-28 05:07:43 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,6 +1,6 @@
-magic_exit_number = 9
-count = 0
-while count < 10 do
+magic_exit_number = 1
+count = 10
+while count > 10 do
   break if count == magic_exit_number
   puts "I am the #{count}, I love to count!" # Work
   count = count + 1
