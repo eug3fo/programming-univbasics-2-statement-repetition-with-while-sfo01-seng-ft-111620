42d66181fc89b0689d0fa55b286ca001a954fd84
Eugene Hong <eugenebhong@gmail.com>
2020-10-28 05:09:15 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -2,7 +2,7 @@ magic_exit_number = 1
 count = 10
 while count > 10 do
   break if count == magic_exit_number
-  puts "#{count}" # Work
+  puts "#{count}"
   count = count - 1
   puts "Happy New Year!"
 end
