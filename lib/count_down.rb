f379dde936e147d341df08fc2691c9c14c59aecb
Eugene Hong <eugenebhong@gmail.com>
2020-10-28 05:10:17 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,8 +1,6 @@
-magic_exit_number = 1
 count = 10
-while count > 10 do
-  break if count == magic_exit_number
-  puts "#{count}"
-  count = count - 1
-  puts "Happy New Year!"
+while count >= 1 do
+  puts count
+  count -= 1
 end
+puts "Happy New Year!"
